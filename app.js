67151cc0f50d1666c8f1205ee5d05d771e3ba8d8
Song minseok <ske3056@gmail.com>
2023-04-01 16:48:00 +0900

    number string boolean

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ function add(num1, num2, isShow, pharse) {
     if (isShow) {
         console.log(pharse + num1 + num2);
         console.log(pharse + result);
+        //string문자형이 앞에 있을 경우 뒤에 숫자형이 문자형으로 변환 '3'+3.3'가 된다.
+        //따라서 숫자형끼리 연산을 마친 뒤 문자열과 연산하도 로직을 짜야 한다.
     }
     else {
         return num1 + num2;
