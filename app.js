bb227fe2a194abf40274753059cabf13c7124dda
Song minseok <ske3056@gmail.com>
2023-04-01 16:33:03 +0900

    javascript vs typescript

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
+function add(num1, num2) {
+    if(typeof num1 !=== "number" || typeof num2!== "number")
+    {
+        throw new Error("Incrrect input!");
+    }
+    return num1 + num2;
+}
+var number1 = '5';
+var number2 = 3.4;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
-console.log("왜 안되냐?!?!?");
+function add(num1:number, num2:number)
+{
+    // if(typeof num1 !== "number" || typeof num2!== "number")
+    // {
+    //     throw new Error("Incrrect input!");
+    // }
+    return num1 + num2;
+}
+
+const number1 = 3;
+const number2 = 3.4;
+
+const result = add(number1, number2);
+
+console.log(result);
