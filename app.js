6fad200d42265450a1d01a60b3eaa9357708193f
Song minseok <ske3056@gmail.com>
2023-04-01 16:44:55 +0900

    number and string and boolean

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-function add(num1, num2) {
-    if(typeof num1 !=== "number" || typeof num2!== "number")
-    {
-        throw new Error("Incrrect input!");
+function add(num1, num2, isShow, pharse) {
+    // if(typeof num1 !== "number" || typeof num2!== "number")
+    // {
+    //     throw new Error("Incrrect input!");
+    // }
+    var result = num1 + num2;
+    if (isShow) {
+        console.log(pharse + num1 + num2);
+        console.log(pharse + result);
     }
-    return num1 + num2;
+    else {
+        return num1 + num2;
+    }
 }
-var number1 = '5';
+var number1 = 3;
 var number2 = 3.4;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is : ";
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,15 +1,26 @@
-function add(num1:number, num2:number)
+function add(num1:number, num2:number, isShow:boolean, pharse:string)
 {
     // if(typeof num1 !== "number" || typeof num2!== "number")
     // {
     //     throw new Error("Incrrect input!");
     // }
-    return num1 + num2;
+    const result:number = num1 + num2;
+    if(isShow)
+    {
+        console.log(pharse + num1 + num2);
+        console.log(pharse + result);
+        //string문자형이 앞에 있을 경우 뒤에 숫자형이 문자형으로 변환 '3'+3.3'가 된다.
+        //따라서 숫자형끼리 연산을 마친 뒤 문자열과 연산하도 로직을 짜야 한다.
+    }
+    else
+    {
+        return num1 + num2;
+    }
 }
 
 const number1 = 3;
 const number2 = 3.4;
+const printResult = true;
+const resultPhrase = "Result is : ";
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
