4fb2ebcc050b1215068e8c00670965948ce8ca4e
Song minseok <ske3056@gmail.com>
2023-04-02 21:52:34 +0900

    array type

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var person
 // }
 = {
     name: "Maximilian",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
+var favoriteActiities; //any 타입은 문자, 숫자 어느 것이든 혼용가능
+favoriteActiities = ["Sports", 1];
 console.log(person.name);
+for (var _i = 0, favoriteActiities_1 = favoriteActiities; _i < favoriteActiities_1.length; _i++) {
+    var hobby = favoriteActiities_1[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -5,7 +5,21 @@ const person
 // }
 = {
     name: "Maximilian",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
 
+let favoriteActiities: any[]; //any 타입은 문자, 숫자 어느 것이든 혼용가능
+favoriteActiities = ["Sports", 1]
 console.log(person.name);
+
+for(const hobby of person.hobbies)
+{
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // map 속성에는 문자열이 존재하지 않는다.!!!Error!!!
+}
+
+for(const hobby of favoriteActiities)
+{
+    console.log(hobby.toUpperCase()); // 숫자일의 경우 런타임 오류 발생함.
+}
