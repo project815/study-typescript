9bb0edc34d3247db1618969858185e2fabf5e1c7
Song minseok <ske3056@gmail.com>
2023-04-07 00:43:55 +0900

    sourceMap

--- analytics.js
+++ analytics.js
@@ -1,2 +1,3 @@
 "use strict";
 console.log("TypeScript is Good");
+//# sourceMappingURL=analytics.js.map
--- app.ts
+++ app.ts
@@ -4,3 +4,15 @@ button?.addEventListener('click', () => {
     console.log("Clicked!!");
 })
 
+
+// const map = new Map();
+
+// const button = document.querySelector('button')!;
+
+// button.addEventListener('click', () => {
+//   const selection = window.getSelection();
+//   if (selection && selection.rangeCount > 0) {
+//     const range = selection.getRangeAt(0);
+//     console.log(range.toString());
+//   }
+// });
