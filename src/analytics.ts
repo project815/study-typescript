7c3bf792e2209863767302ec67cec1a5d912ca17
Song minseok <ske3056@gmail.com>
2023-04-09 20:52:21 +0900

    Chapter3 Compelte

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,1 +1,8 @@
 // console.log("TypeScript is Good");
+
+function sendAnalytics(data:string)
+{
+    console.log(data);
+}
+
+sendAnalytics('This data!');
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
 const button = document.querySelector('button')!;
 
+function clickHandler(message : string)
+{
+    console.log('clicked' + message);
+}
+
 button?.addEventListener('click', () => {
-    console.log("Clicked!!");
+    // let userName = 'Max';  //"noUnusedLocals": true,      
+    console.log("Clicked!!" + clickHandler.bind(null));
 })
 
+function add3(n1 :number, n2:number)  //"noUnusedParameters": true, 
+{
+    if(n1 +n2>0)
+    {
+        return n1+n2;
+    }
+    return;
+}
+
+const button2 = document.querySelector('button2');
+if(button2)
+{
+    button.addEventListener('click', () => {
+        console.log("Clicked!!");
+    })
+}
 
 // const map = new Map();
 
